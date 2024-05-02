3f0ba9aa16d6fb38db2c661f21e90b72aa820335
Jolin <b07209039@gmail.com>
2024-05-02 17:36:12 +0800

    comment of unit

--- calculation.py
+++ calculation.py
@@ -17,8 +17,8 @@ class constants:
 def saturation_vapor_pressure(T, Tunit = "K"):
     """estimate saturation vapor pressure (es)
     es(T) = 6.1094 * e^(17.625T/(243.04+T))
-    T unit: K
-    es unit: Pa
+    T unit: degC
+    es unit: hPa
     recommended by Alduchov and Eskridge (1996) https://doi.org/10.1175/1520-0450(1996)035<0601:IMFAOS>2.0.CO;2
     :param T: temperature
     :param Tunit: unit of temperature K or degC
