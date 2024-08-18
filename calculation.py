76450956756e6190e33e7dbcd990a9731dbd4657
Jolin <b07209039@gmail.com>
2024-08-18 20:55:03 +0800

    add \n  to commend of funcs

--- calculation.py
+++ calculation.py
@@ -5,12 +5,12 @@ import numpy as np
 
 
 def saturation_vapor_pressure(T, Tunit = "K"):
-    """estimate saturation vapor pressure (es)
-    es(T) = 6.1094 * e^(17.625T/(243.04+T))
-    T unit: degC
-    es unit: hPa
-    recommended by Alduchov and Eskridge (1996) https://doi.org/10.1175/1520-0450(1996)035<0601:IMFAOS>2.0.CO;2
-    :param T: temperature
+    """estimate saturation vapor pressure (es)\n
+    es(T) = 6.1094 * e^(17.625T/(243.04+T))\n
+    T unit: degC\n
+    es unit: hPa\n
+    recommended by Alduchov and Eskridge (1996) https://doi.org/10.1175/1520-0450(1996)035<0601:IMFAOS>2.0.CO;2\n
+    :param T: temperature\n
     :param Tunit: unit of temperature K or degC
     """
     T = Tunitconversion(T, Tunit, aimunit = "degC")
@@ -23,15 +23,15 @@ def saturation_mixingratio(T, P, Tunit = "K", Punit = "Pa"):
 
 def mixingratio_from_pressure(e, P):
     """
-    :param e: vapor pressure
+    :param e: vapor pressure\n
     :param P: air pressure
     """
     return constants.epsilon*e/(P-e)
 
 def vapor_pressure_from_mixingratio(qv, P, qvunit = "kg/kg", Punit = "Pa"):
     """
-    qv: mixingratio
-    P: air pressure
+    qv: mixingratio\n
+    P: air pressure\n
     return: vapor pressure (Pa)
     """
     P  = Punitconversion(P, Punit, aimunit="Pa")
@@ -43,13 +43,13 @@ def specific_humidity_from_mixingratio(qv, qvunit = "kg/kg"):
     return qv / (1 + qv)
 
 def potential_temperature(T, P, P_ref = 100000, Tunit = "K", Punit = "Pa", P_refunit = "Pa"):
-    """estimate potential temperature (adiabatic lifting of downwelling to P_base)
-    theta = T * (100000 / P) ^ (Rd / Cp)
-    T unit: K
-    P unit: Pa
-    :param T: temperature
-    :param Tunit: unit of temperature K or degC
-    :param P: pressure
+    """estimate potential temperature (adiabatic lifting of downwelling to P_base)\n
+    theta = T * (100000 / P) ^ (Rd / Cp)\n
+    T unit: K\n
+    P unit: Pa\n
+    :param T: temperature\n
+    :param Tunit: unit of temperature K or degC\n
+    :param P: pressure\n
     :param Punit: unit of pressure Pa or hPa
     """
     T = Tunitconversion(T, Tunit, aimunit = "K")
@@ -67,10 +67,10 @@ def wswd_to_uv(ws, wd, wdunit = "rad", wdtype = "met"):
 
 def calculate_geopotential_height(P, T, e, Tunit = "K", Punit = "Pa", eunit = "Pa"):
     """
-    P: pressure
-    T: temperature
-    e: water vapor pressure
-    rho: density
+    P: pressure\n
+    T: temperature\n
+    e: water vapor pressure\n
+    rho: density\n
     z: geopotential height
     """
     T  = Tunitconversion(T, Tunit, aimunit="K")
@@ -88,9 +88,9 @@ def calculate_geopotential_height(P, T, e, Tunit = "K", Punit = "Pa", eunit = "P
 
 def calculate_LCL(P, T, qv, dP, Tunit = "K", Punit = "Pa", qvunit = "kg/kg"):
     """
-    P: pressure
-    dP: 10 times of pressure resolution
-    T: temperature
+    P: pressure\n
+    dP: 10 times of pressure resolution\n
+    T: temperature\n
     qv: water vapor mixingratio
     """
     T  = Tunitconversion(T, Tunit, aimunit="K")
@@ -117,8 +117,8 @@ def calculate_LCL(P, T, qv, dP, Tunit = "K", Punit = "Pa", qvunit = "kg/kg"):
 
 def sea_level_pressure(T, P, z, lapse_rate, z_ref = 0, Tunit="K", Punit="Pa"):
     """
-    z: meter
-    lapse_rate: K / meter
+    z: meter\n
+    lapse_rate: K / meter\n
     return slp (Pa)
     """
     TK = Tunitconversion(T, nowunit=Tunit, aimunit="K")
@@ -146,7 +146,7 @@ def equivalent_potential_temperature(T, P, qv, Tunit="K", Punit="Pa", qvunit="kg
 
 def column_water_vapor(P, qv, Punit="Pa", qvunit="kg/kg", zaxis = 0):
     """
-    also called precipitable water
+    also called precipitable water\n
     zaxis: which axis (dim) is for height (or pressure)
     """
     P  = Punitconversion(P, nowunit=Punit, aimunit="Pa")
