87e6f7904271ab21cc4225dfc6c2bca2976e188e
Jolintw <b07209039@gmail.com>
2026-05-15 19:20:07 +0800

    modify variable name

--- calculation_withmypkgs.py
+++ calculation_withmypkgs.py
@@ -20,8 +20,8 @@ def vorticity(u, v, x_1D, y_1D, xaxis = -1, yaxis = -2, ndiff = 2):
         _central_diff = central_diff_4thorder
     dudy = _central_diff(u, y_1D, yaxis, broadX=True)
     dvdx = _central_diff(v, x_1D, xaxis, broadX=True)
-    div = - dudy + dvdx
-    return div
+    vor = - dudy + dvdx
+    return vor
 
 def gradient(var, x_1D, y_1D, xaxis = -1, yaxis = -2, ndiff = 2):
     if ndiff == 2:
